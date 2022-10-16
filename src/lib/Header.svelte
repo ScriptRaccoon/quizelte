d3335ffd28cad8612230b6b2a4227a92bb44a83b
Script Raccoon <scriptraccoon@gmail.com>
2022-10-16 11:59:05 +0200

    github link with logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,11 @@
 <header>
+	<a
+		href="https://github.com/ScriptRaccoon/quizelte"
+		rel="noreferrer"
+		target="_blank"
+	>
+		<img src="./github-logo.svg" alt="github logo" /></a
+	>
 	<h1>Quizelte</h1>
 	<small>A quiz about web development</small>
 </header>
@@ -14,6 +21,21 @@
 		small {
 			text-decoration: underline;
 			text-underline-offset: 4px;
+		}
+		a {
+			position: absolute;
+			top: 10px;
+			right: 10px;
+			color: white;
+			text-decoration: none;
+			opacity: 0.8;
+			&:hover,
+			&:focus-visible {
+				opacity: 1;
+			}
+			img {
+				width: 18px;
+			}
 		}
 	}
 </style>
